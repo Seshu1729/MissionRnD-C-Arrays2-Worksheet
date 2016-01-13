4e465063f8700847c60094952930cf96da8475f2
Seshu1729 <Seshu1729@missionrnd.com>
2016-01-13 12:25:06 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -13,6 +13,21 @@ ERROR CASES: Return -1 for invalid inputs.
 NOTES:
 */
 
-int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+#include <iostream>
+
+int findSingleOccurenceNumber(int *A, int len)
+{
+	if (A == NULL)
+		return -1;
+	int index;
+	int appear_one_time = 0, appear_two_times = 0, not_three;
+	for (index = 0; index < len; index++)
+	{
+		appear_two_times |= appear_one_time & A[index];
+		appear_one_time ^= A[index];
+		not_three = ~(appear_one_time&appear_two_times);
+		appear_one_time &= not_three;
+		appear_two_times &= not_three;
+	}
+	return appear_one_time;
 }

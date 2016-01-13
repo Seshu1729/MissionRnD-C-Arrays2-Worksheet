fee797046afe268715f403c9f8278240ecce78d1
Seshu1729 <Seshu1729@missionrnd.com>
2016-01-13 09:55:26 +0530

    Submit Commit

--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -16,12 +16,43 @@ NOTES:
 
 #include <iostream>
 
-struct transaction {
+struct transaction 
+{
 	int amount;
 	char date[11];
 	char description[20];
 };
 
-struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+int isEqual(char *original, char *expected)
+{
+	while (*original&&*expected)
+	{
+		if (*original != *expected)
+			return 0;
+		original++;
+		expected++;
+	}
+	if (!(*original || *expected))
+		return 1;
+	else
+		return 0;
+}
+
+struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) 
+{
+	struct transaction *result = NULL;
+	int i, j, RLen = 0;
+	for (i = 0; i < ALen && A!=NULL; i++)
+	{
+		for (j = i; j < BLen && B!=NULL; j++)
+		{
+			if (isEqual(A[i].date, B[j].date))
+			{
+				RLen++;
+				result = (struct transaction *)realloc(result, RLen*sizeof(struct transaction));
+				result[RLen - 1] = A[i];
+			}
+		}
+	}
+	return result;
 }
